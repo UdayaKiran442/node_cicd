0c32178274ab5b48a93d1047460f523760857c18
UdayaKiran442 <gudaya2002@gmail.com>
2024-05-05 12:47:35 +0530

    gg

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
